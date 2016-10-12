fce7b8e613a77d19f84c9e15be024109f480f80f
Hackbright Student <no-reply@hackbrightacademy.com>
2016-10-12 12:59:14 -0700

    Further study almost complete

--- madlibs.py
+++ madlibs.py
@@ -1,4 +1,4 @@
-from random import choice
+from random import choice, sample, randint
 
 from flask import Flask, render_template, request
 
@@ -10,7 +10,7 @@ app = Flask(__name__)
 AWESOMENESS = [
     'awesome', 'terrific', 'fantastic', 'neato', 'fantabulous', 'wowza', 'oh-so-not-meh',
     'brilliant', 'ducky', 'coolio', 'incredible', 'wonderful', 'smashing', 'lovely']
-
+MADLIBTEMPLATES = ['madlib.html', 'madlib2.html']
 
 @app.route('/')
 def start_here():
@@ -32,7 +32,7 @@ def greet_person():
 
     player = request.args.get("person")
 
-    compliment = choice(AWESOMENESS)
+    compliment = sample(AWESOMENESS, randint(1, len(AWESOMENESS)))
 
     return render_template("compliment.html",
                            person=player,
@@ -58,8 +58,9 @@ def show_madlib():
     noun = request.args.get("noun")
     adjective = request.args.get("adjective")
     adject2 = request.args.getlist("adject2")
+    madlib_result = choice(MADLIBTEMPLATES)
 
-    return render_template("madlib.html", name=name, color=color, 
+    return render_template(madlib_result, name=name, color=color, 
                                     noun=noun, adjective=adjective, adject2=adject2)
 
 
