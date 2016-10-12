13d3343043e550a0b6a18d33677b811cc0431b45
Hackbright Student <no-reply@hackbrightacademy.com>
2016-10-11 18:01:40 -0700

    fun with madlibs

---
+++ madlibs.py
@@ -0,0 +1,69 @@
+from random import choice
+
+from flask import Flask, render_template, request
+
+
+# "__name__" is a special Python variable for the name of the current module
+# Flask wants to know this to know what any imported things are relative to.
+app = Flask(__name__)
+
+AWESOMENESS = [
+    'awesome', 'terrific', 'fantastic', 'neato', 'fantabulous', 'wowza', 'oh-so-not-meh',
+    'brilliant', 'ducky', 'coolio', 'incredible', 'wonderful', 'smashing', 'lovely']
+
+
+@app.route('/')
+def start_here():
+    """Homepage."""
+
+    return "Hi! This is the home page."
+
+
+@app.route('/hello')
+def say_hello():
+    """Save hello to user."""
+
+    return render_template("hello.html")
+
+
+@app.route('/greet')
+def greet_person():
+    """Greet user."""
+
+    player = request.args.get("person")
+
+    compliment = choice(AWESOMENESS)
+
+    return render_template("compliment.html",
+                           person=player,
+                           compliment=compliment)
+
+@app.route('/game')
+def play_game():
+    """Play game."""
+    # player = request.args.get("person")
+    game_response = request.args.get("response")
+
+    if game_response == "yes":
+        return render_template("game.html")
+    else:
+        return render_template("goodbye.html")
+
+@app.route('/madlib')
+def show_madlib():
+    """Make a madlib."""
+    # player = request.args.get("person")
+    name = request.args.get("name")
+    color = request.args.get("color")
+    noun = request.args.get("noun")
+    adjective = request.args.get("adjective")
+    adject2 = request.args.getlist("adject2")
+
+    return render_template("madlib.html", name=name, color=color, 
+                                    noun=noun, adjective=adjective, adject2=adject2)
+
+
+if __name__ == '__main__':
+    # debug=True gives us error messages in the browser and also "reloads" our web app
+    # if we change the code.
+    app.run(debug=True)
